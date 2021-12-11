c4927ec72348da58f979ec9d02c64889c0452a7b
Alex Richards <alexchristopherrichards@gmail.com>
2021-12-11 12:43:31 -0500

    Workshop Week 1

--- ReactWeek1Assignment.js
+++ ReactWeek1Assignment.js
@@ -7,16 +7,28 @@ class Student {
 
 }
 class Bootcamp {
-    constructor(name, level, students) {
+    constructor(name, level, students = []) {
         this.name = name;
         this.level = level;
-        this.students = students;
-
-        if (students === null) {
-            students = [];
-        }
+        this.students = [students];
     }
     registerStudent(student) {
-        const emailExists = this.students.map(email => email === student);
+        this.students.find(s => {
+            if (s.email !== student.email) {
+                this.students.push(student);
+                console.log(`Registering ${student.email} to the bootcamp.`)
+            } else {
+                console.log(`${student.email} is already registered.`);
+            }
+        });
+        return this.students;
     }
-}
+} 
+
+const student1 = new Student('bob', 'bob@gmail.com', 'East');
+const student2 = new Student('mary', 'mary@gmail.com', 'West');
+const bootcamp1 = new Bootcamp('Nucamp', 'react');
+
+console.log(bootcamp1.registerStudent(student1));
+console.log(bootcamp1.registerStudent(student2));
+console.log(bootcamp1.registerStudent(student2));
