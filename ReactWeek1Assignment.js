a458e0970b33e8baf7c6dbeace1343d075f66b44
Alex Richards <alexchristopherrichards@gmail.com>
2021-12-11 09:44:05 -0500

    first commit

---
+++ ReactWeek1Assignment.js
@@ -0,0 +1,22 @@
+class Student {
+    constructor(name, email, community) {
+        this.name = name;
+        this.email = email;
+        this.community = community;
+    }
+
+}
+class Bootcamp {
+    constructor(name, level, students) {
+        this.name = name;
+        this.level = level;
+        this.students = students;
+
+        if (students === null) {
+            students = [];
+        }
+    }
+    registerStudent(student) {
+        const emailExists = this.students.map(email => email === student);
+    }
+}
