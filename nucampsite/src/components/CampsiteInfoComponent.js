a9fa61f743cfd2f70066818f7725b3947c5ab91e
Alex Richards <alexchristopherrichards@gmail.com>
2022-01-08 05:09:16 -0500

    Fetch Post Comment

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -24,7 +24,7 @@ function RenderCampsite({campsite}) {
   );
 }
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
   if (comments) {
     return (
       <div className="col-md-5 m-1">
@@ -40,7 +40,7 @@ function RenderComments({comments, addComment, campsiteId}) {
             }).format(new Date(Date.parse(comment.date)))}
           </div>
         ))}
-        <CommentForm campsiteId={campsiteId} addComment={addComment} />
+        <CommentForm campsiteId={campsiteId} postComment={postComment} />
       </div>
     );
   }
@@ -66,7 +66,7 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
     this.toggleModal();
-    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+    this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
   }
 
   render() {
@@ -184,7 +184,7 @@ function CampsiteInfo(props) {
           <RenderCampsite campsite={props.campsite} />
           <RenderComments 
             comments={props.comments} 
-            addComment={props.addComment}
+            postComment={props.postComment}
             campsiteId={props.campsite.id}
           />
         </div>
